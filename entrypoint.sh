e032e0206b5877c5a49853ed35fec250482e1a5b
Sajal Gupta <sajal.gupta@zomato.com>
2020-04-02 13:20:25 +0530

    blocking on title wip occurence
    printing labels to debug and scan

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,17 @@ set -e
 
 # skip if not a PR
 echo "Checking if a PR command..."
-(jq .)
-echo $GITHUB_EVENT_PATH
 (jq -r ".pull_request.url" "$GITHUB_EVENT_PATH") || exit 78
 
+#printing labels
+(jq -r ".pull_request.labels" "$GITHUB_EVENT_PATH") 
+
 # Block if wip found in title
-echo "Checking if contains WIP ..."
-(jq .)
-(jq -r ".pull_request.title" "$GITHUB_EVENT_PATH" | grep -E "wip") exit 1 && exit 0
+echo "Checking wip mentions in title ..."
+result=$(jq -r ".pull_request.title" "$GITHUB_EVENT_PATH")
+if [[ $result =~ "wip" ]]
+then
+   exit 1
+else
+   exit 0
+fi
