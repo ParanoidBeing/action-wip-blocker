3948c0bb741b044995b1f03348825e3eef6722e1
Sajal Gupta <sajal.gupta@zomato.com>
2020-04-02 11:28:28 +0530

    Updating block rules

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,10 @@ set -e
 # skip if not a PR
 echo "Checking if a PR command..."
 (jq .)
+echo $GITHUB_EVENT_PATH
 (jq -r ".pull_request.url" "$GITHUB_EVENT_PATH") || exit 78
 
-# skip if no /revert
-echo "Checking if contains '/revert' command..."
+# Block if wip found in title
+echo "Checking if contains WIP ..."
 (jq .)
-(jq -r ".pull_request.title" "$GITHUB_EVENT_PATH" | grep -E "wip") || exit 78
+(jq -r ".pull_request.title" "$GITHUB_EVENT_PATH" | grep -E "wip") && exit 78
