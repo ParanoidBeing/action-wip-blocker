72ea3d09a826148d083b5ff281b75a04d29d4dfc
Sajal Gupta <sajal.gupta@zomato.com>
2020-04-02 14:34:45 +0530

    Fix: Provide json to jq

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ set -e
 echo "Checking if a PR command..."
 (jq -r ".pull_request.url" "$GITHUB_EVENT_PATH") || exit 78
 
-title=$(jq -r ".pull_request.title")
-labels=$(jq -r ".pull_request.labels")
+title=$(jq -r ".pull_request.title" "$GITHUB_EVENT_PATH")
+labels=$(jq -r ".pull_request.labels" "$GITHUB_EVENT_PATH")
 
 checkForBlockingWords(){
     if echo $1 | grep -iE 'WIP|do not merge|backend not live'
